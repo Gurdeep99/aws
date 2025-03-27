123bf80312e185e5d0673a7932c0bc4cbfa55d77
Gurdeep Singh <grdp99@gmail.com>
2025-03-27 16:17:46 +0530

    fix

--- app/api/mj/home/route.js
+++ app/api/mj/home/route.js
@@ -7,25 +7,38 @@ export async function GET(req) {
   // Fetch query parameters
   const { searchParams } = new URL(req.url);
   const limit = searchParams.get("limit") || "10"; // Default value 10
-  const skip = searchParams.get("skip") || "0";   // Default value 0
+  const skip = searchParams.get("skip") || "0"; // Default value 0
 
-  if(authHeader !== "Bearer 117f34ce-8402-4003-a6de-cd0d28b4c216"){
-    return new Response(JSON.stringify({ message : "Unauthorized"}), { status: 401, headers: { "Content-Type": "application/json" } });
+  if (authHeader !== "Bearer 117f34ce-8402-4003-a6de-cd0d28b4c216") {
+    return new Response(JSON.stringify({ message: "Unauthorized" }), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  function getBannerData() {
+    const bannerData = [
+      "https://aws.gurdeep.net/mj/banner.png",
+      "https://aws.gurdeep.net/mj/banner2.png",
+      "https://aws.gurdeep.net/mj/banner3.png",
+      "https://aws.gurdeep.net/mj/banner4.png",
+    ];
+
+    const randomIndex = Math.floor(Math.random() * bannerData.length);
+
+    return bannerData[randomIndex];
   }
 
   const data = {
-    banner : "https://aws.gurdeep.net/mj/banner.png",
+    banner: getBannerData(),
     tools: {
-      panchang : "https://www.myjyotish.com/panchang/aaj-ka-panchang",
-      horoscope : "https://www.myjyotish.com/horoscope",
-      kundli : "https://www.myjyotish.com/tags/kundli",
+      panchang: "https://www.myjyotish.com/panchang/aaj-ka-panchang",
+      horoscope: "https://www.myjyotish.com/horoscope",
+      kundli: "https://www.myjyotish.com/tags/kundli",
     },
-    vedic : jyotish(),
-    astro : jyotish()
+    vedic: jyotish(),
+    astro: jyotish(),
   };
-
-
-
 
   return Response.json(data);
 }
