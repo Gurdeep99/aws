6a1c7777f71d8d7698ccd77b5350a3e180175182
Gurdeep Singh <grdp99@gmail.com>
2025-05-29 15:28:41 +0530

    fix

--- app/api/mj/[slug]/route.js
+++ app/api/mj/[slug]/route.js
@@ -4,15 +4,7 @@ export async function GET(req, { params }) {
     const { slug } = params;
 
     try {
-        await axios.post(`https://ssostage.amarujala.com/v1/chat/vendor_status`, {
-            chat_id: slug,
-            status: 'completed',
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        await axios.post(`http://ssostage.amarujala.com/v1/chat/status`, {
+        const response1 = await axios.post(`https://ssostage.amarujala.com/v1/chat/vendor_status`, {
             chat_id: slug,
             status: 'completed',
         }, {
@@ -21,10 +13,26 @@ export async function GET(req, { params }) {
             },
         });
 
-        return new Response("Weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' },
+        const response2 = await axios.post(`http://ssostage.amarujala.com/v1/chat/status`, {
+            chat_id: slug,
+            status: 'completed',
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
         });
+
+        if (response1.status === 200 && response2.status === 200) {
+            return new Response("Weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", {
+                status: 200,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        } else {
+            return new Response("Munna nahi hoga tum se", {
+                status: 500,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
     } catch (error) {
         console.error('Error fetching:', error.message);
         return new Response("Munna nahi hoga tum se", {
