0cbc7ac24f2d0efc83b08c940d87f44f6bf6e403
Gurdeep Singh <grdp99@gmail.com>
2025-07-07 10:46:25 +0530

    m

--- app/api/au/home/route.js
+++ app/api/au/home/route.js
@@ -55,14 +55,14 @@ export async function POST(request) {
 
     // Return the added data as a response
     return new Response(JSON.stringify({
-        "feedId": 2269,
+    "feedId": 2269,
     "feedVersionId": 2270,
     "userId": "6741d0e7c722f07def054cc",
     "sessionId": "c60e11e1-d2cb-4d3f-85f6-11e3f13cda9c",
     "pageId": "766946f5-73e7-420b-b76b-48fe2156977a",
     "pageNo": 1,
-        "items": data,
-        "feedTitle": null
+    "items": data,
+    "feedTitle": null
     }), {
       headers: { 'Content-Type': 'application/json' },
     });
