16016821ef4a8480b9b9c80c441e86a91ea6d8b5
Gurdeep Singh <grdp99@gmail.com>
2025-07-07 10:51:12 +0530

    a

--- app/api/au/home/route.js
+++ app/api/au/home/route.js
@@ -14,8 +14,8 @@ const shuffleArray = (array) => {
 // GET method to return shuffled data
 export async function GET(request) {
   try {
-    // Path to the data.json file inside the public folder
-    const filePath = path.resolve(process.cwd(), 'public', 'data.json');
+    // Path to the data.json file inside src/data/
+    const filePath = path.resolve(process.cwd(), 'src', 'data', 'data.json');
     
     // Read the data from the file
     const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
@@ -25,14 +25,14 @@ export async function GET(request) {
 
     // Return the shuffled data as a JSON response
     return new Response(JSON.stringify({
-    "feedId": 2269,
-    "feedVersionId": 2270,
-    "userId": "6741d0e7c722f07def054cc",
-    "sessionId": "c60e11e1-d2cb-4d3f-85f6-11e3f13cda9c",
-    "pageId": "766946f5-73e7-420b-b76b-48fe2156977a",
-    "pageNo": 1,
-    "items": shuffledData,
-    "feedTitle": null
+      "feedId": 2269,
+      "feedVersionId": 2270,
+      "userId": "6741d0e7c722f07def054cc",
+      "sessionId": "c60e11e1-d2cb-4d3f-85f6-11e3f13cda9c",
+      "pageId": "766946f5-73e7-420b-b76b-48fe2156977a",
+      "pageNo": 1,
+      "items": shuffledData,
+      "feedTitle": null
     }), {
       headers: { 'Content-Type': 'application/json' },
     });
@@ -50,8 +50,8 @@ export async function POST(request) {
   try {
     const data = await request.json();
 
-    // Path to the data.json file inside the public folder
-    const filePath = path.resolve(process.cwd(), 'public', 'data.json');
+    // Path to the data.json file inside src/data/
+    const filePath = path.resolve(process.cwd(), 'src', 'data', 'data.json');
     
     // Read existing data
     const existingData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
@@ -64,14 +64,14 @@ export async function POST(request) {
 
     // Return the added data as a response
     return new Response(JSON.stringify({
-    "feedId": 2269,
-    "feedVersionId": 2270,
-    "userId": "6741d0e7c722f07def054cc",
-    "sessionId": "c60e11e1-d2cb-4d3f-85f6-11e3f13cda9c",
-    "pageId": "766946f5-73e7-420b-b76b-48fe2156977a",
-    "pageNo": 1,
-    "items": data,
-    "feedTitle": null
+      "feedId": 2269,
+      "feedVersionId": 2270,
+      "userId": "6741d0e7c722f07def054cc",
+      "sessionId": "c60e11e1-d2cb-4d3f-85f6-11e3f13cda9c",
+      "pageId": "766946f5-73e7-420b-b76b-48fe2156977a",
+      "pageNo": 1,
+      "items": [data],
+      "feedTitle": null
     }), {
       headers: { 'Content-Type': 'application/json' },
     });
