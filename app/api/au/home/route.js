2e36371894536b8ad4637f079bfbcafd221ef2d1
Gurdeep Singh <grdp99@gmail.com>
2025-07-07 10:49:47 +0530

    a

--- app/api/au/home/route.js
+++ app/api/au/home/route.js
@@ -24,7 +24,16 @@ export async function GET(request) {
     const shuffledData = shuffleArray(data);
 
     // Return the shuffled data as a JSON response
-    return new Response(JSON.stringify(shuffledData), {
+    return new Response(JSON.stringify({
+    "feedId": 2269,
+    "feedVersionId": 2270,
+    "userId": "6741d0e7c722f07def054cc",
+    "sessionId": "c60e11e1-d2cb-4d3f-85f6-11e3f13cda9c",
+    "pageId": "766946f5-73e7-420b-b76b-48fe2156977a",
+    "pageNo": 1,
+    "items": shuffledData,
+    "feedTitle": null
+    }), {
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
