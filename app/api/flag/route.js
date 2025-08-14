17989060b895d217ccd5348543ec0519ceee197f
Gurdeep Singh <grdp99@gmail.com>
2025-08-14 16:24:06 +0530

    fix

--- app/api/flag/route.js
+++ app/api/flag/route.js
@@ -4,11 +4,7 @@ import axios from "axios";
 export async function GET(req) {
   // Fetch Authorization header
   const authHeader = req.headers.get("authorization");
-
-  const { searchParams } = new URL(req.url);
-  const skip = searchParams.get("skip") || 0; // Default value 10
-  const limit = searchParams.get("limit") || 10; // Default value 10
-
+ 
   if (authHeader !== "Bearer 117f34ce-8402-4003-a6de-cd0d28b4c216") {
     return new Response(JSON.stringify({ message: "Unauthorized" }), {
       status: 401,
